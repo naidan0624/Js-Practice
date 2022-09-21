12d159c4921743e1d443fc309f3085765029e82b
Naidan Natsagdorj <jessicana2424@gmail.com>
2022-09-20 20:55:30 -0500

    JAVASCRIPT

---
+++ conditional.js
@@ -0,0 +1,20 @@
+//function ifElse(number1, number2){
+//if(number1 > number2) {
+//  console.log(number1)
+// } else { 
+// console.log(number2)
+//}
+//}
+//ifElse(-3, -1)
+ 
+
+
+
+function findOldAge(num1,num2){
+    if (num1>num2){
+        console.log(num2)
+    } else[
+        console.log(num1)
+    ]
+}
+findOldAge(50,20)
